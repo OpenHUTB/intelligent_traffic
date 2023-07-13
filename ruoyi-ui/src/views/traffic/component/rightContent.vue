26ed68c8f35c9e28edf817cc6c371aa0311735f8
chengchangli <cccmon@163.com>
2023-07-13 14:52:44 +0800

    视频播放器引入

--- ruoyi-ui/src/views/traffic/component/rightContent.vue
+++ ruoyi-ui/src/views/traffic/component/rightContent.vue
@@ -4,7 +4,7 @@
       <h2>视频监控</h2>
       <div style="width: 29vw; height: 260px" @click="SeeVideo">
           <!-- <video autoplay="autoplay" width="100%" height="100%">
-              <source src="../../../assets/webm/identify.mp4" type="video/mp4"/>
+              <source src="../../../assets/webm/CAMERAV.webm" type="video/mp4"/>
           </video> -->
           <video-player class="video-player vjs-custom-skin" 
               ref="videoPlayer" 
@@ -45,7 +45,7 @@ export default {
       playerOptions: {
         playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
         autoplay: true, // 如果为true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
+        muted: true, // 默认情况下将会消除任何音频。
         loop: false, // 是否视频一结束就重新开始。
         preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
         language: 'zh-CN',
@@ -53,7 +53,8 @@ export default {
         fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
         sources: [{
           type: "video/mp4", // 类型
-          src: '/video/identify.mp4' // url地址
+          //src: '/video/identify.mp4' // url地址
+          src: '/video/CAMERAV.webm' // url地址
         }],
         poster: '', // 封面地址
         notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
