bd2e1fda06ae7587af9c8fca3d4170e67cee61a5
chengchangli <cccmon@163.com>
2023-07-17 17:43:10 +0800

    增加一些交通点线面的实体类，引入mybatis-plus

---
+++ ruoyi-system/src/main/java/com/ruoyi/traffic/domain/TrafficArea.java
@@ -0,0 +1,35 @@
+package com.ruoyi.traffic.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.ruoyi.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @classname: TrafficArea
+ * @author: chengchangli
+ * @description: 路网面的类
+ * @date: 2023/7/17
+ * @version: v1.0
+ **/
+@Data
+public class TrafficArea extends BaseEntity implements Serializable {
+
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty("主键ID")
+    private Long id;
+
+    @ApiModelProperty("大区编号+小区编号")
+    private String code;
+
+    @ApiModelProperty("面积大小")
+    private BigDecimal area;
+
+    @ApiModelProperty("用地类型")
+    private String landUseType;
+
+}
---
+++ ruoyi-system/src/main/java/com/ruoyi/traffic/domain/TrafficLine.java
@@ -0,0 +1,35 @@
+package com.ruoyi.traffic.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.ruoyi.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @classname: TrafficLine
+ * @author: chengchangli
+ * @description: 路网线的类
+ * @date: 2023/7/17
+ * @version: v1.0
+ **/
+@Data
+public class TrafficLine extends BaseEntity implements Serializable {
+
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty("主键ID")
+    private Long id;
+
+    @ApiModelProperty("道路名称")
+    private String name;
+
+    @ApiModelProperty("道路起点ID")
+    private Long startPointId;
+
+    @ApiModelProperty("道路终点ID")
+    private Long endPointId;
+
+
+}
