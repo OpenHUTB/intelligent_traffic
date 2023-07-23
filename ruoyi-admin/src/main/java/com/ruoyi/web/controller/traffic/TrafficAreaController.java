6fcc6bd36299ecb224265a6e9311fdb3f98a7b4b
chengchangli <cccmon@163.com>
2023-07-23 09:41:02 +0800

    新增一些点线面的controller与service实现方法

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/traffic/TrafficAreaController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/traffic/TrafficAreaController.java
@@ -1,16 +1,18 @@
 package com.ruoyi.web.controller.traffic;
 
 import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.traffic.domain.TrafficArea;
+import com.ruoyi.traffic.domain.TrafficLine;
 import com.ruoyi.traffic.service.ITrafficAreaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -28,5 +30,41 @@ public class TrafficAreaController extends BaseController {
     @Resource
     private ITrafficAreaService trafficAreaService;
 
+    @ApiOperation("分页获取路网的面")
+    @PostMapping("/page")
+    public TableDataInfo list(@ApiParam(value = "条件查询参数")@RequestBody TrafficArea trafficArea)
+    {
+        startPage();
+        List<TrafficArea> list = trafficAreaService.queryList(trafficArea);
+        return getDataTable(list);
+    }
 
+    @ApiOperation("新增路网的面")
+    @PostMapping("/add")
+    public AjaxResult add (@RequestBody TrafficArea trafficArea) {
+        trafficAreaService.addArea(trafficArea);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("编辑路网的面")
+    @PostMapping("/update")
+    public AjaxResult update (@RequestBody TrafficArea trafficArea) {
+        trafficAreaService.updateArea(trafficArea);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("删除路网的面")
+    @PostMapping("delete")
+    public AjaxResult delete(@ApiParam(value = "数据集ID集合", required = true) @RequestBody List<Long> ids) {
+        trafficAreaService.deleteByIdList(ids);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("路网的面的详情")
+    @GetMapping("findById/{id}")
+    public AjaxResult findById(@PathVariable @ApiParam(name = "id")
+                               @NotNull(message = "不能为空") Long id) {
+        TrafficArea trafficArea = trafficAreaService.queryById(id);
+        return AjaxResult.success(trafficArea);
+    }
 }
--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/traffic/TrafficLineController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/traffic/TrafficLineController.java
@@ -1,12 +1,19 @@
 package com.ruoyi.web.controller.traffic;
 
 import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.traffic.domain.TrafficLine;
+import com.ruoyi.traffic.domain.TrafficPoint;
 import com.ruoyi.traffic.service.ITrafficLineService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @classname: TrafficLineController
@@ -22,4 +29,43 @@ public class TrafficLineController extends BaseController {
 
     @Resource
     private ITrafficLineService trafficLineService;
+
+
+    @ApiOperation("分页获取路网的线")
+    @PostMapping("/page")
+    public TableDataInfo list(@ApiParam(value = "条件查询参数")@RequestBody TrafficLine trafficLine)
+    {
+        startPage();
+        List<TrafficLine> list = trafficLineService.queryList(trafficLine);
+        return getDataTable(list);
+    }
+
+    @ApiOperation("新增路网的线")
+    @PostMapping("/add")
+    public AjaxResult add (@RequestBody TrafficLine trafficLine) {
+        trafficLineService.addLine(trafficLine);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("编辑路网的线")
+    @PostMapping("/update")
+    public AjaxResult update (@RequestBody TrafficLine trafficLine) {
+        trafficLineService.updateLine(trafficLine);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("删除路网的线")
+    @PostMapping("delete")
+    public AjaxResult delete(@ApiParam(value = "数据集ID集合", required = true) @RequestBody List<Long> ids) {
+        trafficLineService.deleteByIdList(ids);
+        return AjaxResult.success();
+    }
+
+    @ApiOperation("路网的线的详情")
+    @GetMapping("findById/{id}")
+    public AjaxResult findById(@PathVariable @ApiParam(name = "id")
+                               @NotNull(message = "不能为空") Long id) {
+        TrafficLine trafficLine = trafficLineService.queryById(id);
+        return AjaxResult.success(trafficLine);
+    }
 }
