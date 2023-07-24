4a40faeb8689104fc3ff0feae58f1adefbb1bfe5
OuyangDelong <3100792800@qq.com>
2023-07-24 18:34:09 +0800

    新增路口和路口评价指标的类

---
+++ ruoyi-system/src/main/java/com/ruoyi/traffic/mapper/TrafficIntersectionMapper.java
@@ -0,0 +1,14 @@
+package com.ruoyi.traffic.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.traffic.domain.TrafficIntersection;
+
+/**
+ * @classname: TrafficIntersectionMapper
+ * @author: ouyangdelong
+ * @description: 路口Mapper类
+ * @date: 2023/7/24
+ * @version: v1.0
+ **/
+public interface TrafficIntersectionMapper extends BaseMapper<TrafficIntersection> {
+}
