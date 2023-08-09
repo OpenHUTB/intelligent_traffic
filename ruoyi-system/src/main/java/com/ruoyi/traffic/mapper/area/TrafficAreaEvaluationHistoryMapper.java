f8439e2e37f546e9f82043c3d7a96bb10492a494
15516257496 <1972281311@qq.com>
2023-08-09 14:43:50 +0800

    完成区域实时评价和区域历史评价的控制类，区域历史评价的指标排名，新增区域历史评价的vo

--- ruoyi-system/src/main/java/com/ruoyi/traffic/mapper/area/TrafficAreaEvaluationHistoryMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/traffic/mapper/area/TrafficAreaEvaluationHistoryMapper.java
@@ -1,13 +1,13 @@
 package com.ruoyi.traffic.mapper.area;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+
+import com.github.yulichang.base.MPJBaseMapper;
 import com.ruoyi.traffic.domain.area.TrafficAreaEvaluationHistory;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
+
+
 
 
 /**
@@ -18,17 +18,5 @@ import java.util.List;
  * @version: v1.0
  **/
 @Mapper
-public interface TrafficAreaEvaluationHistoryMapper extends BaseMapper<TrafficAreaEvaluationHistory> {
-//   @Select("SELECT *, RANK() OVER (ORDER BY value DESC) AS rank " +
-//           "FROM traffic_area_evaluation_history " +
-//           "WHERE evaluation_type_id = #{evaluation_type_id} " +
-//           "ORDER BY value DESC " +
-//           "LIMIT 20")
-@Select( "SELECT VALUE RANK() OVER ( ORDER BY VALUE DESC ) AS myrank " +
-        "FROM traffic_area_evaluation_history "+
-    "WHERE evaluation_type_id =#{evaluation_type_id} LIMIT 20"
-           )
-
-
-    List<TrafficAreaEvaluationHistory> getEvaluationRankById(@Param("evaluation_type_id") Long evaluationTypeId);
+public interface TrafficAreaEvaluationHistoryMapper extends MPJBaseMapper<TrafficAreaEvaluationHistory> {
 }
