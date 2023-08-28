f82fc44674536de7c385a46282aeffd9dc1cfb39
oddlove <odd_love@sina.cn>
2023-08-28 15:03:49 +0800

    添加视频人脸进行语音播报

--- ruoyi-simulation/src/main/java/com/ruoyi/simulation/controller/DocumentController.java
+++ ruoyi-simulation/src/main/java/com/ruoyi/simulation/controller/DocumentController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.swing.text.Document;
 
 /**
  * 文件信息业务层接口
--- ruoyi-simulation/src/main/java/com/ruoyi/simulation/util/FileUtil.java
+++ ruoyi-simulation/src/main/java/com/ruoyi/simulation/util/FileUtil.java
@@ -8,8 +8,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 /**
  * 文件处理工具类
@@ -25,21 +25,74 @@ public class FileUtil {
      * @return
      * @throws IOException
      */
-    public synchronized AjaxResult storeFileToDisk(byte[] blob) {
-        AjaxResult result = null;
+    public synchronized String storeFileToDisk(byte[] blob) {
+        String absolutePath = null;
         try {
-            String targetPath = environment.getProperty("simulation.filepath")+ File.separator+System.currentTimeMillis()+".wav";
-            File targetFile = new File(targetPath);
+            File targetFile = this.createDefaultFile(".wav");
+            FileUtils.writeByteArrayToFile(targetFile, blob);
+            absolutePath = targetFile.getAbsolutePath();
+        } catch (IOException e) {
+            logger.error(LoggerUtil.getLoggerStace(e));
+            throw new RuntimeException("保存话筒语音失败!");
+        }
+        return absolutePath;
+    }
+
+    /**
+     *
+     * @param inputStream
+     * @return
+     */
+    public synchronized String storeFileToDisk(InputStream inputStream, String suffix){
+        String fileName = null;
+        try {
+            File targetFile = this.createDefaultFile(suffix);
+            FileUtils.copyInputStreamToFile(inputStream, targetFile);
+            fileName = targetFile.getName();
+        } catch (IOException e) {
+            logger.error(LoggerUtil.getLoggerStace(e));
+        }
+        return fileName;
+    }
+
+    /**
+     * 创建默认文件
+     * @return
+     */
+    public File createDefaultFile(String suffix){
+        //指定语音文件的生成位置
+        String targetPath = environment.getProperty("simulation.filepath")+ File.separator+System.currentTimeMillis()+suffix;
+        File targetFile = new File(targetPath);
+        //判断父目录是否存在，若不存在则创建父目录
+        if (!targetFile.getParentFile().exists()) {
+            targetFile.getParentFile().mkdirs();
+        }
+        return targetFile;
+    }
+
+    /**
+     * 向文件中写入文本
+     * @param absolutePath
+     * @param lineList
+     */
+    public void writeText(String absolutePath, List<String> lineList) throws IOException {
+        BufferedWriter bufferedWriter = null;
+        try {
+            File targetFile = new File(absolutePath);
             //判断父目录是否存在，若不存在则创建父目录
             if (!targetFile.getParentFile().exists()) {
                 targetFile.getParentFile().mkdirs();
             }
-            FileUtils.writeByteArrayToFile(targetFile, blob);
-            result = AjaxResult.success(targetFile);
-        } catch (IOException e) {
-            logger.error(LoggerUtil.getLoggerStace(e));
-            result = AjaxResult.error("保存录音文件失败!");
+            FileOutputStream ous = new FileOutputStream(targetFile);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(ous,"gbk"));
+            for(String line: lineList){
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+        } finally {
+            if(bufferedWriter!=null){
+                bufferedWriter.close();
+            }
         }
-        return result;
     }
 }
