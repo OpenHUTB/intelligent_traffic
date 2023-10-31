3a87d6847f2ca1a1d682f8d714547cfb12adb17b
simon <ouyangyly@gmail.com>
2023-10-31 20:38:19 +0800

    add the page for junction scenario and setting react router

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/App.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Main';
-
+import PlanMain from './components/PlanMain';
+import JunctionMain from './components/JunctionMain';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 export default function App() {
   return (
-    <div id="layout">
-        <Header/>
-        <Main/>
-    </div>
+    <Router>
+      <div id="layout">
+        <Header />
+        <Routes>
+          <Route path="/" element={<PlanMain />} />
+          <Route path="/junction" element={<JunctionMain />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
