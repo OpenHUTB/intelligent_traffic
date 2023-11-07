1ba45be32d49a00ee10c6171bfd6d026978be381
simon <ouyangyly@gmail.com>
2023-11-07 18:39:43 +0800

    junction view lights control optimization style and strategy

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/index.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import "./css/index.scss"
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
