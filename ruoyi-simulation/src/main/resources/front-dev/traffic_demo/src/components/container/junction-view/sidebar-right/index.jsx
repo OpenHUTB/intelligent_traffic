4823de73363a81f915553f91c44a2afe770c3153
simon <ouyangyly@gmail.com>
2023-11-08 21:05:39 +0800

    junction view first version finish, version 0.1

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/sidebar-right/index.jsx
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/sidebar-right/index.jsx
@@ -1,9 +1,13 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { ReactComponent as LeftIcon } from '../../../../assets/icon/icon-left.svg';
 import { ReactComponent as ForwardIcon } from '../../../../assets/icon/icon-forward.svg';
 
 import { ReactComponent as NavIcon } from '../../../../assets/icon/icon-nav.svg';
+import option1 from '../../../../assets/img/scheme1.png';
+import option2 from '../../../../assets/img/scheme2.png';
+import option3 from '../../../../assets/img/scheme3.png';
+
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -77,8 +81,66 @@ export default function Index() {
     };
 
 
+    // pedestrain optimization list
+    const scrollContainer = useRef(null);
+    useEffect(() => {
+        const startAutoScroll = () => {
+            const container = scrollContainer.current;
+            const scrollAmount = 2.5; // Adjust for faster/slower scrolling
 
+            const interval = setInterval(() => {
+                // When you've scrolled to the end of the original content, reset to the top
+                if (container.scrollTop >= (container.scrollHeight / 2)) {
+                    container.scrollTop = 0; // Set to start without user noticing
+                } else {
+                    container.scrollTop += scrollAmount;
+                }
+            }, 50); // Adjust the interval for faster/slower scrolling
 
+            return () => clearInterval(interval); // Cleanup on component unmount
+        }
+
+        startAutoScroll();
+    }, []);
+    const staticListItems = [{
+        name: "二次过街",
+        index: "否",
+    },
+    {
+        name: "人机冲突指数",
+        index: "52",
+    },
+    {
+        name: "人非冲突指数",
+        index: "54",
+    },
+    {
+        name: "人均延误",
+        index: "65s",
+    },
+    {
+        name: "服务水平",
+        index: "C",
+    },
+    {
+        name: "等待空间",
+        index: "B",
+    },
+    {
+        name: "过街空间",
+        index: "1",
+    },
+    ];
+
+    const renderList = staticListItems.map((item, index) => {
+        return (
+            <div className="list-item" key={index}>
+                <span className="name">{item.name}</span>
+                <span className="index">{item.index}</span>
+            </div>
+        )
+    });
+    console.log(renderList);
     return (
         <section className="slider-right">
 
@@ -152,6 +214,36 @@ export default function Index() {
 
 
             </div>
+
+            {/* predestrain optimization */}
+            <div className="pedestrain-optimization-container">
+                <div className="title"><span className='svg'><NavIcon /></span><span>行人过街设置优化</span></div>
+                <main className="pedestrian-main">
+                    <section className="pedestrain-scroll-container">
+                        <div className='nav-container'>
+                            <span>行人过街指标</span>
+                            <span>评价</span>
+                        </div>
+                        <div className="list-container" ref={scrollContainer}>
+                            {renderList}
+                            {renderList}
+                        </div>
+                    </section>
+                    <section className="pedestrian-index">
+                        <span className="number">71</span>
+                        <span className="description">行人过街综合评分</span>
+                    </section>
+                </main>
+            </div>
+            <section className="options-comparison">
+                <div className="title"><span className='svg'><NavIcon /></span><span>最佳方案</span></div>
+
+                <div className='video-container'>
+                    <img src={option1} alt="" />
+                    <img src={option2} alt="" />
+                    <img src={option3} alt="" />
+                </div>
+            </section>
         </section>
     );
 };
