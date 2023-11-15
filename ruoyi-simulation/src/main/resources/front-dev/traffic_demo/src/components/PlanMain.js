64947a5f66db29b66f2881bb066d51c4e79233ca
simon <ouyangyly@gmail.com>
2023-11-15 15:05:10 +0800

    traffic jam rank component done

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/PlanMain.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/PlanMain.js
@@ -2,6 +2,7 @@ import Centre from 'components/Centre';
 import TrafficFlow from 'components/container/plan-view/sidebar-left/TrafficFlow/TrafficFlow';
 import TrafficJam from 'components/container/plan-view/bottom/TrafficJam/TrafficJam';
 import AverageDelay from 'components/container/plan-view/bottom/AverageDelay/AverageDelay';
+import TrafficRank from 'components/container/plan-view/bottom/TrafficRank/TrafficRank';
 import '../css/plan.scss';
 
 
@@ -19,6 +20,7 @@ export default function Main() {
       <div className='bottomSide'>
         <TrafficJam />
         <AverageDelay />
+        <TrafficRank />
       </div>
     </main>
   );
