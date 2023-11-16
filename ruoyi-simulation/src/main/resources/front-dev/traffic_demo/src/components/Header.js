d03aee63d8d2f854f62b7bf64a8848d529e5522e
simon <ouyangyly@gmail.com>
2023-11-16 17:40:46 +0800

    plan view layout finished

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/Header.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
             <ul className='nav-left'>
                 <li><Link to='/'>路况概览</Link></li>
                 <li><Link to='/Junction'>路口场景</Link></li>
-                <li></li>
+                <li>车辆跟踪</li>
             </ul>
             <div className="title">
                 <h2 className="title-1">智慧交通大模型</h2>
