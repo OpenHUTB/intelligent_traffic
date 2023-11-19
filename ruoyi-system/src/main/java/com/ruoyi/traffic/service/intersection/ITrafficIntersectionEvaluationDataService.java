f42fbfd4563123c7efe6cb18e514cfce53801ffb
oyh <1972281311@qq.com>
2023-11-19 12:10:12 +0800

    获取文本每行内容测试

--- ruoyi-system/src/main/java/com/ruoyi/traffic/service/intersection/ITrafficIntersectionEvaluationDataService.java
+++ ruoyi-system/src/main/java/com/ruoyi/traffic/service/intersection/ITrafficIntersectionEvaluationDataService.java
@@ -44,4 +44,7 @@ public interface ITrafficIntersectionEvaluationDataService extends IService<Traf
     //新增路网仿真数据
     void addData(JSONArray jsonArray) throws JSONException;
 
+    //读取文本每行内容
+    List readTxtFile(String filePath, String encoding);
+
 }
