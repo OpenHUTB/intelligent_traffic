f9a01ab8d250373559811862ad54eede3a920f7a
simon <ouyangyly@gmail.com>
2023-11-27 19:29:58 +0800

    using glb file instead of fbx for stable performance

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/TextArea/TextArea.jsx
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/TextArea/TextArea.jsx
@@ -1,23 +1,56 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
-import Stats from 'three/addons/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { TGALoader } from 'three/examples/jsm/loaders/TGALoader.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './index.scss';
 
-export default function TextArea() {
+export default function TextArea(props) {
+    console.log(props);
     const animationRef = useRef(null);
+    // handle the animation play and hide.
+    const [isPlay, setIsPlay] = useState(true);
+    const getAnimationClassName = () => {
+        return `text-container ${isPlay ? 'play' : ''}`
+    }
+    const inactivePosition = { right: "20%", bottom: "0", width: "10rem", height: "10rem" };
+    const activePosition = { left: "50%", top: "65%", transform: "translate(-50%, -50%)", width: "30rem", height: "30rem" };
+    const [position, setPosition] = useState(activePosition);
+
+    const handleClick = (event) => {
+        const { clientX, clientY } = event;
+        const { innerWidth, innerHeight } = window;
+        const margin = 50; // Define corner area
+        const isCornerClick = (
+            (clientX < margin && clientY < margin) ||
+            (clientX > innerWidth - margin && clientY < margin) ||
+            (clientX < margin && clientY > innerHeight - margin) ||
+            (clientX > innerWidth - margin && clientY > innerHeight - margin)
+        );
+
+        if (isCornerClick) {
+            setIsPlay(prevState => !prevState); // Toggle animation visibility
+            setPosition(prevState => {
+                if (prevState.width === inactivePosition.width) {
+                    return activePosition;
+                } else {
+                    return inactivePosition;
+                }
+            });
+        }
+    }
+
 
     useEffect(() => {
         let camera, scene, renderer;
         const clock = new THREE.Clock();
         let mixer;
-
+        window.addEventListener('click', handleClick);
         function init() {
             camera = new THREE.PerspectiveCamera(45, animationRef.current.clientWidth / animationRef.current.clientHeight, 1, 2000);
-            camera.position.set(0, 100, 300);
+            camera.position.set(0, 0, 0);
+
 
             scene = new THREE.Scene();
             scene.background = null;
@@ -34,63 +67,37 @@ export default function TextArea() {
             // Add an ambient light
             const ambientLight = new THREE.AmbientLight(0xffffff, 2); // soft white light
             scene.add(ambientLight);
-            // // Ground
-            // const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
-            // mesh.rotation.x = -Math.PI / 2;
-            // mesh.receiveShadow = true;
-            // scene.add(mesh);
 
             // Model loading fbx
-            const manager = new THREE.LoadingManager();
-            manager.addHandler(/\.tga$/i, new TGALoader());
-            const loader = new FBXLoader(manager);
-            loader.load('IP_test/IP_Anim01.fbx', function (object) {
-                console.log(object)
-                // object.mixer = new THREE.AnimationMixer(object);
-                console.log(object.animations)
+            // const manager = new THREE.LoadingManager();
+            // manager.addHandler(/\.tga$/i, new TGALoader());
+            // const loader = new FBXLoader(manager);
+            // Model loading glb
+            const loader = new GLTFLoader();
+            loader.load('IP_test/IP_Anim_test01.glb', function (gltf) {
+                const model = gltf.scene;
+                console.log(model);
+                mixer = new THREE.AnimationMixer(model);
+                gltf.animations.forEach((clip) => {
+                    mixer.clipAction(clip).play();
+                });
 
-                mixer = new THREE.AnimationMixer(object);
-                const action = mixer.clipAction(object.animations[0]);
-                action.timeScale = 0.8
-                action.play();
-
-                object.traverse(function (child) {
+                model.traverse(function (child) {
                     if (child.isMesh) {
                         child.castShadow = true;
                         child.receiveShadow = true;
                     }
                 });
-                object.scale.set(3, 3, 3);
-                scene.add(object);
+
+                if (isPlay) {
+                    model.scale.set(3, 3, 3);
+                } else {
+                    model.scale.set(2, 2, 2);
+                    console.log(model.scale);
+                }
+                scene.add(model);
 
             });
-            // model loading gltf
-            // const manager = new THREE.LoadingManager();
-            // manager.addHandler(/\.tga$/i, new TGALoader());
-            // const loader = new GLTFLoader(manager);
-            // loader.load('animations/IP_out/IP.gltf', function (gltf) {
-            //     const model = gltf.scene;
-            //     console.log(gltf);
-            //     console.log(gltf.animations); // 打印动画数组
-
-            //     mixer = new THREE.AnimationMixer(model);
-            //     if (gltf.animations.length) {
-            //         const action = mixer.clipAction(gltf.animations[0]);
-            //         action.timeScale = 0.8;
-            //         action.play();
-            //     }
-
-            //     model.traverse(function (child) {
-            //         if (child.isMesh) {
-            //             child.castShadow = true;
-            //             child.receiveShadow = true;
-            //         }
-            //     });
-
-            //     model.scale.set(2.5, 2.5, 2.5); // 根据需要调整比例
-            //     scene.add(model);
-            // });
-
 
             renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
             renderer.setPixelRatio(window.devicePixelRatio);
@@ -133,12 +140,13 @@ export default function TextArea() {
             if (animationRef.current) {
                 animationRef.current.removeChild(renderer.domElement);
             }
+            window.removeEventListener('click', handleClick)
         };
-    }, []);
+    }, [isPlay]);
 
     return (
-        <section className="voiceDetect">
-            <div className='text-container'><span id="tips">Hello，我是小轩</span></div>
+        <section className="voiceDetect" style={position}>
+            <div className={getAnimationClassName()}><textarea id="tips" defaultValue={"Hello 我是小轩"}></textarea></div>
             <div id="AnimationIP" ref={animationRef}></div>
         </section>
     );
