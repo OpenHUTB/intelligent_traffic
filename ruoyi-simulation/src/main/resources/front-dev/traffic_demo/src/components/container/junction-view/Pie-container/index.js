2f67979886a0fd30f79cfdcc12e1c798b9ecebd6
simon <ouyangyly@gmail.com>
2023-12-17 22:57:34 +0800

    set new junction layout

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/Pie-container/index.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/Pie-container/index.js
@@ -1,31 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setJunctioninfo } from 'stores/junctionInfoSlice';
 import { ReactComponent as NavIcon } from 'assets/icon/icon-nav.svg';
 import { ReactComponent as Car } from 'assets/icon/car.svg';
 import { ReactComponent as LineUp } from 'assets/icon/Lineup1.svg';
 import { ReactComponent as Parking } from 'assets/icon/parking.svg';
 import { ReactComponent as Total } from 'assets/icon/time2.svg';
-import { ReactComponent as C } from 'assets/icon/C3.svg';
-export default function index() {
+export default function Index() {
+    const dispatch = useDispatch();
+    const junctionInfo = useSelector((state) => state.junctionInfo);
+    console.log(junctionInfo);
+
+    useEffect(() => {
+        const handleJunctionInfo = (event) => {
+            console.log(event.detail);
+            const key = Object.keys(event.detail)[0];
+            const value = event.detail[key];
+            console.log(key, value);
+            dispatch(setJunctioninfo({ [key]: value }));
+        };
+        window.addEventListener('junctionInfoChanged', handleJunctionInfo);
+        return () => {
+            window.removeEventListener('junctionInfoChanged', handleJunctionInfo);
+        }
+    }, [junctionInfo, dispatch])
     //set the traffic monitor pie chart;
-    const Counter = ({ counts, time = 500 }) => {
-        const [count, setCount] = useState(0);
-        useEffect(() => {
-            let startTime = Date.now();
-            let duration = 2000;
-            const timer = setInterval(() => {
-                setCount(() => {
-                    let after = ((Date.now() - startTime) / duration * counts).toFixed(1);
-                    if (after > counts) {
-                        clearInterval(timer);
-                        after = counts;
-                    }
-                    return after;
-                });
-            }, 16);
-            return () => clearInterval(timer);
-        }, [counts]);
-        return count;
-    }
+    // const Counter = ({ counts, time = 500 }) => {
+    //     const [count, setCount] = useState({junctionInfo});
+    //     useEffect(() => {
+    //         let startTime = Date.now();
+    //         let duration = time;
+    //         const timer = setInterval(() => {
+    //             setCount(() => {
+    //                 let after = ((Date.now() - startTime) / duration * counts).toFixed(1);
+    //                 if (after > counts) {
+    //                     clearInterval(timer);
+    //                     after = counts;
+    //                 }
+    //                 return after;
+    //             });
+    //         }, 16);
+    //         return () => clearInterval(timer);
+    //     }, [counts]);
+    //     return count;
+    // }
     return (
         <div className="pie-container">
             <div className="title"><span className='svg'><NavIcon /></span><span>路口运行信息</span></div>
@@ -40,11 +58,13 @@ export default function index() {
                     <span>平均延误</span>
                 </div>
                 <div className="textData">
-                    <span id='grade'><C /></span>
-                    <span className="number"><Counter counts={20.7} /><span className="unit">m</span></span>
-                    <span className="number"><Counter counts={15} /><span className="unit">次</span></span>
-                    <span className="number"><Counter counts={23.5} /><span className="unit">s</span></span>
-
+                    <span id='grade' className='number'>{junctionInfo.grade}</span>
+                    {/* <span className="number"> <Counter counts={20.7} /> <span className="unit">m</span></span>
+                    <span className="number"><Counter counts={15} /> <span className="unit">次</span></span>
+                    <span className="number"><Counter counts={23.5} /> <span className="unit">s</span></span> */}
+                    <span className="number"> {junctionInfo.queueLength}<span className="unit">m</span></span>
+                    <span className="number"> {junctionInfo.parkingTimes} <span className="unit">次</span></span>
+                    <span className="number"> {junctionInfo.averageDelay} <span className="unit">s</span></span>
                 </div>
             </div>
         </div>
--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/sidebar-left/index.jsx
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/junction-view/sidebar-left/index.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './style.scss';
-
 import TrafficFlow from '../Traffic-Flow/';
 import PieContainer from '../Pie-container/';
 import LineContainer from '../Line-container/';
 import Overview from '../Traffic-Overview/Overview';
+import PedestrainOptimize from '../Pedestrain-optimize';
 export default function Index(props) {
 
 
@@ -12,10 +12,11 @@ export default function Index(props) {
 
     return (
         <section className="junction-leftTop">
-            {/* <TrafficFlow />
             <PieContainer />
-            <LineContainer /> */}
-            <Overview />
+            {/* <TrafficFlow /> */}
+            <LineContainer />
+            {/* <Overview /> */}
+            <PedestrainOptimize />
         </section >
 
     )
