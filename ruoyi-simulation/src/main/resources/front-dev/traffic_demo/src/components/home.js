6a9375db153f438fa6130f9449608e4024417fc1
simon <ouyangyly@gmail.com>
2024-04-09 18:33:35 +0800

    new nav bar style for the home page

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/home.js
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/home.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import twinVideo from '../assets/videos/twin-video.mp4';
-import '../css/plan.scss';
+import styles from 'css/home.module.scss';
 
 export default function Home() {
 
     return (
         <main>
             <div id="twinContainer">
-                <video id="twin" muted src={twinVideo} ></video>
+                {/* <video id="twin" muted src={twinVideo} ></video> */}
             </div>
-            {/* <Centre /> */}
-            <div className="rightSide">
+            <div className={styles.leftSide}>
             </div>
-            <div className='bottomSide'>
-
+            <div className={styles.rightSide}>
+            </div>
+            <div className={styles.bottomSide}>
             </div>
         </main>
     );
