7d0f9583dcbf76e5f5f59c8f1bef6b8b0d819326
simon <ouyangyly@gmail.com>
2024-06-03 09:45:22 +0800

    change the layout

--- ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/layout/highway/RightSide.jsx
+++ ruoyi-simulation/src/main/resources/front-dev/traffic_demo/src/components/container/layout/highway/RightSide.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import OperationDetect from './OperationDetect'
+import Map from '../homeview/Map/Map'
 export default function RightSide() {
     return (
         <div>
             <OperationDetect />
+            <Map />
         </div>
     )
 }
