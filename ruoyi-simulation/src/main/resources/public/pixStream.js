53d669a1dcb8bf3d1b7f04f353f4b2f49a8b34ff
summer-y <odd_love@sina.cn>
2024-11-17 11:52:10 +0800

    获取大屏参数

--- ruoyi-simulation/src/main/resources/public/pixStream.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var ws = connect();
-    var interval = setInterval(function() {
-        if (ws.readyState == 2 || ws.readyState == 3) {
-            ws = connect();
-        }
-    },500);
-});
